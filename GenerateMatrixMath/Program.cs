884609fd6ab599d66ac5e93722f485774488cbe1
John Gietzen <john@gietzen.us>
2025-07-02 14:30:03 -0700

    Casts and Extension properties.

--- GenerateMatrixMath/Program.cs
+++ GenerateMatrixMath/Program.cs
@@ -8,6 +8,22 @@
     {
         static void Main()
         {
+            var casts = new[]
+            {
+                typeof(Half),
+                typeof(float),
+                typeof(double),
+                typeof(decimal),
+                typeof(byte),
+                typeof(short),
+                typeof(int),
+                typeof(long),
+                typeof(sbyte),
+                typeof(ushort),
+                typeof(uint),
+                typeof(ulong),
+            };
+
             var outputPaths = new[] {
                 @"C:\Users\otac0n\Projects\Silk.NET\sources\Maths\Maths\",
                 @"C:\Users\otac0n\Projects\GenerateMatrixMath\DemoLibrary\",
@@ -35,121 +51,121 @@
 
             var matrixModels =
                 (from d in dims
-                    select new Matrix(d, dims, multiplicationOperators[d])).ToList();
+                    select new Matrix(d, dims, casts, multiplicationOperators[d])).ToList();
 
             var extensions = new Extension[]
             {
-            // INumber<>
-            new(typeof(INumber<>), nameof(INumber<>.Sign), [Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.Max), [Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.Max), [Memberwise, One]),
-            new(typeof(INumber<>), nameof(INumber<>.MaxNumber), [Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.MaxNumber), [Memberwise, One]),
-            new(typeof(INumber<>), nameof(INumber<>.Min), [Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.Min), [Memberwise, One]),
-            new(typeof(INumber<>), nameof(INumber<>.MinNumber), [Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.MinNumber), [Memberwise, One]),
-            new(typeof(INumber<>), nameof(INumber<>.Clamp), [Memberwise, Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.Clamp), [Memberwise, One, One]),
-            new(typeof(INumber<>), nameof(INumber<>.CopySign), [Memberwise, Memberwise]),
-            new(typeof(INumber<>), nameof(INumber<>.CopySign), [Memberwise, One]),
+                // INumber<>
+                new(typeof(INumber<>), nameof(INumber<>.Sign), [Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.Max), [Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.Max), [Memberwise, One]),
+                new(typeof(INumber<>), nameof(INumber<>.MaxNumber), [Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.MaxNumber), [Memberwise, One]),
+                new(typeof(INumber<>), nameof(INumber<>.Min), [Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.Min), [Memberwise, One]),
+                new(typeof(INumber<>), nameof(INumber<>.MinNumber), [Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.MinNumber), [Memberwise, One]),
+                new(typeof(INumber<>), nameof(INumber<>.Clamp), [Memberwise, Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.Clamp), [Memberwise, One, One]),
+                new(typeof(INumber<>), nameof(INumber<>.CopySign), [Memberwise, Memberwise]),
+                new(typeof(INumber<>), nameof(INumber<>.CopySign), [Memberwise, One]),
 
-            // INumberBase<>
-            new(typeof(INumberBase<>), nameof(INumberBase<>.Abs), [Memberwise]),
-            new(typeof(INumberBase<>), nameof(INumberBase<>.MaxMagnitude), [Memberwise, Memberwise]),
-            new(typeof(INumberBase<>), nameof(INumberBase<>.MaxMagnitudeNumber), [Memberwise, Memberwise]),
-            new(typeof(INumberBase<>), nameof(INumberBase<>.MinMagnitude), [Memberwise, Memberwise]),
-            new(typeof(INumberBase<>), nameof(INumberBase<>.MinMagnitudeNumber), [Memberwise, Memberwise]),
-            //new(typeof(INumberBase<>), nameof(INumberBase<>.MultiplyAddEstimate), [Memberwise, Memberwise, Memberwise]), // DOTNET>=9
-            //new(typeof(INumberBase<>), nameof(INumberBase<>.MultiplyAddEstimate), [Memberwise, One, One]), // DOTNET>=9
+                // INumberBase<>
+                new(typeof(INumberBase<>), nameof(INumberBase<>.Abs), [Memberwise]),
+                new(typeof(INumberBase<>), nameof(INumberBase<>.MaxMagnitude), [Memberwise, Memberwise]),
+                new(typeof(INumberBase<>), nameof(INumberBase<>.MaxMagnitudeNumber), [Memberwise, Memberwise]),
+                new(typeof(INumberBase<>), nameof(INumberBase<>.MinMagnitude), [Memberwise, Memberwise]),
+                new(typeof(INumberBase<>), nameof(INumberBase<>.MinMagnitudeNumber), [Memberwise, Memberwise]),
+                //new(typeof(INumberBase<>), nameof(INumberBase<>.MultiplyAddEstimate), [Memberwise, Memberwise, Memberwise]), // DOTNET>=9
+                //new(typeof(INumberBase<>), nameof(INumberBase<>.MultiplyAddEstimate), [Memberwise, One, One]), // DOTNET>=9
 
-            // IBinaryNumber<>
-            //new(typeof(IBinaryNumber<>), nameof(IBinaryNumber<>.Log2), [Memberwise]),
+                // IBinaryNumber<>
+                //new(typeof(IBinaryNumber<>), nameof(IBinaryNumber<>.Log2), [Memberwise]),
 
-            // IBinaryInteger<>
-            new(typeof(IBinaryInteger<>), nameof(IBinaryInteger<>.PopCount), [Memberwise]),
-            new(typeof(IBinaryInteger<>), nameof(IBinaryInteger<>.TrailingZeroCount), [Memberwise]),
+                // IBinaryInteger<>
+                new(typeof(IBinaryInteger<>), nameof(IBinaryInteger<>.PopCount), [Memberwise]),
+                new(typeof(IBinaryInteger<>), nameof(IBinaryInteger<>.TrailingZeroCount), [Memberwise]),
 
-            // IFloatingPoint<>
-            new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Ceiling), [Memberwise]),
-            new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Floor), [Memberwise]),
-            new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Round), [Memberwise]),
-            new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Round), [Memberwise, One, One]),
-            new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Truncate), [Memberwise]),
+                // IFloatingPoint<>
+                new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Ceiling), [Memberwise]),
+                new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Floor), [Memberwise]),
+                new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Round), [Memberwise]),
+                new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Round), [Memberwise, One, One]),
+                new(typeof(IFloatingPoint<>), nameof(IFloatingPoint<>.Truncate), [Memberwise]),
 
-            // IFloatingPointIeee754<>
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Atan2), [Memberwise, Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Atan2Pi), [Memberwise, Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Lerp), [Memberwise, Memberwise, One]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.BitDecrement), [Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.BitIncrement), [Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.FusedMultiplyAdd), [Memberwise, Memberwise, Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.FusedMultiplyAdd), [Memberwise, One, One]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Ieee754Remainder), [Memberwise, Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Ieee754Remainder), [Memberwise, One]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ILogB), [Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ReciprocalEstimate), [Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ReciprocalSqrtEstimate), [Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ScaleB), [Memberwise, Memberwise]),
-            new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ScaleB), [Memberwise, One]),
+                // IFloatingPointIeee754<>
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Atan2), [Memberwise, Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Atan2Pi), [Memberwise, Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Lerp), [Memberwise, Memberwise, One]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.BitDecrement), [Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.BitIncrement), [Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.FusedMultiplyAdd), [Memberwise, Memberwise, Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.FusedMultiplyAdd), [Memberwise, One, One]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Ieee754Remainder), [Memberwise, Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.Ieee754Remainder), [Memberwise, One]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ILogB), [Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ReciprocalEstimate), [Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ReciprocalSqrtEstimate), [Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ScaleB), [Memberwise, Memberwise]),
+                new(typeof(IFloatingPointIeee754<>), nameof(IFloatingPointIeee754<>.ScaleB), [Memberwise, One]),
 
-            // IPowerFunctions<>
-            new(typeof(IPowerFunctions<>), nameof(IPowerFunctions<>.Pow), [Memberwise, Memberwise]),
-            new(typeof(IPowerFunctions<>), nameof(IPowerFunctions<>.Pow), [Memberwise, One]),
+                // IPowerFunctions<>
+                new(typeof(IPowerFunctions<>), nameof(IPowerFunctions<>.Pow), [Memberwise, Memberwise]),
+                new(typeof(IPowerFunctions<>), nameof(IPowerFunctions<>.Pow), [Memberwise, One]),
 
-            // IRootFunctions<>
-            new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Cbrt), [Memberwise]),
-            new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Sqrt), [Memberwise]),
-            new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.RootN), [Memberwise, One]),
-            new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.RootN), [Memberwise, Memberwise]),
-            new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Hypot), [Memberwise, Memberwise]),
+                // IRootFunctions<>
+                new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Cbrt), [Memberwise]),
+                new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Sqrt), [Memberwise]),
+                new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.RootN), [Memberwise, One]),
+                new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.RootN), [Memberwise, Memberwise]),
+                new(typeof(IRootFunctions<>), nameof(IRootFunctions<>.Hypot), [Memberwise, Memberwise]),
 
-            // ILogarithmicFunctions<>,
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log), [Memberwise]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log), [Memberwise, One]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.LogP1), [Memberwise]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log2), [Memberwise]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log2P1), [Memberwise]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log10), [Memberwise]),
-            new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log10P1), [Memberwise]),
+                // ILogarithmicFunctions<>,
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log), [Memberwise]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log), [Memberwise, One]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.LogP1), [Memberwise]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log2), [Memberwise]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log2P1), [Memberwise]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log10), [Memberwise]),
+                new(typeof(ILogarithmicFunctions<>), nameof(ILogarithmicFunctions<>.Log10P1), [Memberwise]),
 
-            // IExponentialFunctions<>
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp), [Memberwise]),
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.ExpM1), [Memberwise]),
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp2), [Memberwise]),
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp2M1), [Memberwise]),
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp10), [Memberwise]),
-            new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp10M1), [Memberwise]),
+                // IExponentialFunctions<>
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp), [Memberwise]),
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.ExpM1), [Memberwise]),
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp2), [Memberwise]),
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp2M1), [Memberwise]),
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp10), [Memberwise]),
+                new(typeof(IExponentialFunctions<>), nameof(IExponentialFunctions<>.Exp10M1), [Memberwise]),
 
-            //ITrigonometricFunctions<>
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Acos), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AcosPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Asin), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AsinPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Atan), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AtanPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Cos), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.CosPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Sin), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.SinPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Tan), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.TanPi), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.DegreesToRadians), [Memberwise]),
-            new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.RadiansToDegrees), [Memberwise]),
+                //ITrigonometricFunctions<>
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Acos), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AcosPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Asin), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AsinPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Atan), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.AtanPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Cos), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.CosPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Sin), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.SinPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.Tan), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.TanPi), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.DegreesToRadians), [Memberwise]),
+                new(typeof(ITrigonometricFunctions<>), nameof(ITrigonometricFunctions<>.RadiansToDegrees), [Memberwise]),
 
-            //IHyperbolicFunctions<>
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Acosh), [Memberwise]),
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Asinh), [Memberwise]),
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Atanh), [Memberwise]),
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Cosh), [Memberwise]),
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Sinh), [Memberwise]),
-            new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Tanh), [Memberwise]),
+                //IHyperbolicFunctions<>
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Acosh), [Memberwise]),
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Asinh), [Memberwise]),
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Atanh), [Memberwise]),
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Cosh), [Memberwise]),
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Sinh), [Memberwise]),
+                new(typeof(IHyperbolicFunctions<>), nameof(IHyperbolicFunctions<>.Tanh), [Memberwise]),
             };
 
             var sizes = Enumerable.Range(2, 3);
             var vectorModels =
                 (from d in sizes
-                    select new Model.Vector(d, sizes, extensions)).ToList();
+                    select new Model.Vector(d, sizes, casts, extensions)).ToList();
 
             foreach (var outputPath in outputPaths)
             {
